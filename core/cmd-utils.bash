7a7525e642ba425cda63a627c125be4c2523f9bc
Fred TU <frederic.tu@gmail.com>
2016-04-13 22:21:01 +0800

    Add folder recursive rm & tree cmd

--- core/cmd-utils.bash
+++ core/cmd-utils.bash
@@ -6,3 +6,13 @@ mkcd() {
 disksize() {
   df -h
 }
+
+tree() {
+	pwd
+	find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
+}
+
+# recursive rm to all files that has this name
+del() {
+	find . -name $1 -exec rm {} \;
+}
