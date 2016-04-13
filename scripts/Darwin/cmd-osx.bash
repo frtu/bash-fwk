ec4a1879e57e421d09db408df3b8dde0ed67d085
Fred TU <frederic.tu@gmail.com>
2016-04-13 22:24:40 +0800

    BUG : fix typo

--- scripts/Darwin/cmd-osx.bash
+++ scripts/Darwin/cmd-osx.bash
@@ -1,5 +1,5 @@
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles TRUE;killall Finder'
-alias hideFiles='defaults write com.apple.finder AppleShowAllFiles FALSE;killall Finder
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles FALSE;killall Finder'
 
 dos2unix() {
 	cat $1 | tr '\n' '\r'
