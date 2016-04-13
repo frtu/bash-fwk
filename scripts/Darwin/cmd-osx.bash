5b25161006971a581d40879cc586482400ac28f6
Fred TU <frederic.tu@gmail.com>
2016-04-13 22:14:55 +0800

    Add some OSX utilities

---
+++ scripts/Darwin/cmd-osx.bash
@@ -0,0 +1,11 @@
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles TRUE;killall Finder'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles FALSE;killall Finder
+
+dos2unix() {
+	cat $1 | tr '\n' '\r'
+}
+
+dos2unixfile() {
+	mv $1 "$1.bak"
+	cat "$1.bak" | tr '\n' '\r' > $1
+}
