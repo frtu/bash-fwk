fec0d0c53549c43b0c6ad01111a5979f276ba8c8
Fred TU <frederic.tu@gmail.com>
2017-06-24 22:36:28 +0800

    Add creating cmd shortcuts

--- core/cmd-linux.bash
+++ core/cmd-linux.bash
@@ -7,6 +7,13 @@ $HOME/bin
 cdbin(){
   cd $USR_BIN
 }
+binln() {
+	if [ -z "$2" ] ; then
+        echo "Please provide a folder='$1' and a command filename='$2'"
+        return
+  	fi
+	ln -s "$1/$2" "$USR_BIN/$2"	
+}
 
 disksize() {
   df -h
