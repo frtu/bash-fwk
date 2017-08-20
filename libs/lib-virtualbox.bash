394155d1a264cb37e93d81d04bad10c3c1c4731e
Frederic,Tu <frdtu@paypal.com>
2017-08-20 23:09:23 +0800

    Add VM memory change & snapshot capabilities

--- libs/lib-virtualbox.bash
+++ libs/lib-virtualbox.bash
@@ -13,6 +13,7 @@ vboxls() {
 vboxstart() {
   if [ $# -eq 0 ]; then
     echo "Please specify parameters > 'vboxstart IMAGE_NAME'. If you don't know any names run 'vboxls' and look at the first column \"VBOX_INST_NAMES\""
+    echo ""
     vboxls
     return -1
   fi
@@ -22,6 +23,7 @@ vboxstart() {
 vboxstop() {
   if [ $# -eq 0 ]; then
     echo "Please specify parameters > 'vboxstop IMAGE_NAME'. If you don't know any names run 'vboxls' and look at the first column \"VBOX_INST_NAMES\""
+    echo ""
     vboxls
     return -1
   fi
@@ -29,9 +31,27 @@ vboxstop() {
   VBoxManage controlvm $1 poweroff
 }
 
+vboxmemory() {
+  if [ -z "$1" ]; then
+    echo "Please specify IMAGE_NAME parameter > 'vboxmemory IMAGE_NAME MEMORY_MB'. If you don't know any names run 'vboxls' and look at the first column \"VBOX_INST_NAMES\""
+    echo ""
+    vboxls
+    return -1
+  fi
+  if [ -z "$2" ]; then
+    echo "Please specify MEMORY_MB parameter > 'vboxmemory IMAGE_NAME MEMORY_MB' in MB"
+    return -1
+  fi
+
+  local IMAGE_NAME=$1
+  local MEMORY_MB=$2
+
+  VBoxManage modifyvm $IMAGE_NAME --memory $MEMORY_MB
+}
 vboxport() {
   if [ -z "$1" ]; then
     echo "Please specify IMAGE_NAME parameter > 'vboxport IMAGE_NAME PORT'. If you don't know any names run 'vboxls' and look at the first column \"VBOX_INST_NAMES\""
+    echo ""
     vboxls
     return -1
   fi
@@ -48,10 +68,25 @@ vboxport() {
   #VBoxManage modifyvm $IMAGE_NAME natpf1 "tcp-port$PORT,tcp,,$PORT,,$PORT";
   VBoxManage controlvm $IMAGE_NAME natpf1 "tcp-port$PORT,tcp,,$PORT,,$PORT";
 }
+vboxsnapshot() {
+  if [ $# -eq 0 ]; then
+    echo "Please specify parameters > 'vboxsnapshot IMAGE_NAME [SNAPSHOT_NAME] [SNAPSHOT_DESC]'. If you don't know any names run 'vboxls' and look at the first column \"VBOX_INST_NAMES\""
+    echo ""
+    vboxls
+    return -1
+  fi
+
+  local IMAGE_NAME=$1
+  local SNAPSHOT_NAME=${2:-$1-snapshot}
+  local SNAPSHOT_DESC=${3:-This is the snapshot of $1}
+  echo "VBoxManage snapshot $IMAGE_NAME take $SNAPSHOT_NAME --description \"$SNAPSHOT_DESC\""
+  VBoxManage snapshot $IMAGE_NAME take $SNAPSHOT_NAME --description "$SNAPSHOT_DESC"
+}
 
 vboxrmsoft() {
   if [ $# -eq 0 ]; then
     echo "Please specify parameters > 'vboxrm IMAGE_HASH'. If you don't know any names run 'vboxls' and look at the first column \{INST_HASH\}"
+    echo ""
     vboxls
     return -1
   fi
