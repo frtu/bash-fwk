30a68f91e8d223b65b6e571f796d6bde13d889d8
TU Frederic <frederic.tu@gmail.com>
2017-11-18 14:36:00 +0800

    Create lib-file-transfer with SCP, SSH echo > tee Complete the new cmd-remote

--- core/cmd-remoting.bash
+++ core/cmd-remoting.bash
@@ -1,82 +1,18 @@
-wgetlazy() {
-  if [ $# -eq 0 ]; then
-      echo "wgetlazy FILENAME HTTP_URL [HTTP_PARAMS]"
-      exit 1
-  fi
+import lib-file-transfer
 
-  #if [ ! -z "$3" ]; then
-      #HTTP_PARAMS=--header "$3"
-  #fi
+USR_KEYS=~/.ssh/authorized_keys
 
-  # Download file ONLY if it doesn't exist
-  if [ ! -f $1 ]
-    then
-      echo "wget --no-cookies --no-check-certificate -O $1 $HTTP_PARAMS $2"
-      wget --no-cookies --no-check-certificate -O $1 --header "$3" $HTTP_PARAMS $2
-    else
-      echo "File exist, use this one $1"
-  fi
-}
-
-scppushcore() {
+pushcore() {
   if [ $# -eq 0 ]; then
     echo "Please profide a fully qualified hostname as first parameter!"
-    exit 1
+    return 1
   fi
 
-  if [ -f "~/.ssh/authorized_keys" ]; then
+  if [ -f "$USR_KEYS" ]; then
     echo "ssh -o StrictHostKeyChecking=no $USER@$1 'mkdir -p ~/.ssh/'"
     ssh -o StrictHostKeyChecking=no $USER@$1 'mkdir -p ~/.ssh/'
-    scppush $1 ~/.ssh/authorized_keys "~/.ssh/"
+    
+    trscppush $1 $USR_KEYS "~/.ssh/"
   fi
-  scppush $1 ~/.bash_profile "~/"
-}
-
-scpget() {
-  if [ -z "$3" ]
-    then
-        local FIRST_FOLDER=$2
-        local SECOND_FOLDER=$2
-    else
-        local FIRST_FOLDER=$2
-        local SECOND_FOLDER=$3
-  fi
-  echo scp $USER@$1:$FIRST_FOLDER "$SECOND_FOLDER"
-  sudo scp $USER@$1:$FIRST_FOLDER "$SECOND_FOLDER"
-}
-scpgetdir() {
-  if [ -z "$3" ]
-    then
-        local FIRST_FOLDER=$2
-        local SECOND_FOLDER=$2
-    else
-        local FIRST_FOLDER=$2
-        local SECOND_FOLDER=$3
-  fi
-  echo scp -r $USER@$1:$FIRST_FOLDER "$SECOND_FOLDER"
-  sudo scp -r $USER@$1:$FIRST_FOLDER "$SECOND_FOLDER"
-}
-scppush() {
-  if [ -z "$3" ]
-    then
-        local FIRST_FOLDER=$2
-        local SECOND_FOLDER=$2
-    else
-        local FIRST_FOLDER=$2
-        local SECOND_FOLDER=$3
-  fi
-  echo scp "$FIRST_FOLDER" $USER@$1:$SECOND_FOLDER
-  sudo scp "$FIRST_FOLDER" $USER@$1:$SECOND_FOLDER
-}
-scppushdir() {
-  if [ -z "$3" ]
-    then
-        local FIRST_FOLDER=$2
-        local SECOND_FOLDER=$2
-    else
-        local FIRST_FOLDER=$2
-        local SECOND_FOLDER=$3
-  fi
-  echo scp -r "$FIRST_FOLDER" $USER@$1:$SECOND_FOLDER
-  sudo scp -r "$FIRST_FOLDER" $USER@$1:$SECOND_FOLDER
+  trscppush $1 ~/.bash_profile "~/"
 }
--- libs/lib-file-transfer.bash
+++ libs/lib-file-transfer.bash
@@ -1,26 +1,109 @@
-trwgetlazy() {
+# Only download if target doesn't exist
+trwgetlazy() { 
   if [ $# -eq 0 ]; then
       echo "wgetlazy FILENAME HTTP_URL [HTTP_PARAMS]"
-      exit 1
+      return 1
+  fi
+  if [ -f $1 ]; then
+      echo "File exist, use this one $1"
+      return 1
   fi
 
   #if [ ! -z "$3" ]; then
       #HTTP_PARAMS=--header "$3"
   #fi
 
-  # Download file ONLY if it doesn't exist
-  if [ ! -f $1 ]
-    then
-      echo "wget --no-cookies --no-check-certificate -O $1 $HTTP_PARAMS $2"
-      wget --no-cookies --no-check-certificate -O $1 --header "$3" $HTTP_PARAMS $2
-    else
-      echo "File exist, use this one $1"
+  echo "wget --no-cookies --no-check-certificate -O $1 $HTTP_PARAMS $2"
+  wget --no-cookies --no-check-certificate -O $1 --header "$3" $HTTP_PARAMS $2
+}
+# Download using "*_downloading" extension, and remove only if succeed
+trwgetsafe() {
+  echo "Downloading : wget -O $1 $2"
+  wget -O $1_downloading $2
+  mv $1_downloading $1  
+  echo "Done : $1"
+}
+
+# Using echo & tee over SSH to transfer a file
+trsshpush() {
+  local SSH_FULL_HOSTPATH=$1
+  local LOCAL_RESOURCE=$2
+
+  if [ -z "$SSH_FULL_HOSTPATH" ]; then
+    echo "Please specify required SSH_FULL_HOSTPATH parameters > 'trsshpush SSH_FULL_HOSTPATH LOCAL_RESOURCE'."
+    echo "SSH_FULL_HOSTPATH can be IP, SSH_HOSTNAME or USER@SSH_HOSTNAME"
+    return -1
+  fi
+
+  if [ -d "$LOCAL_RESOURCE" ]; then
+    echo "== Copying directory $LOCAL_RESOURCE by concat into a single execution instruction COMMAND =="
+    echo "ATTENTION : has some bugs not all the files are copied, may need to retry UNSUCCESFUL files ONLY"
+
+    COMMAND=`{ printf "mkdir -p $LOCAL_RESOURCE"; }`
+    for file in $LOCAL_RESOURCE* ; do 
+      COMMAND=`{ printf %s "$COMMAND"; echo -e "\ntee $file <<EOF"; cat $file; echo "EOF"; }`
+    done
+
+    printf %s "$COMMAND" | ssh $SSH_FULL_HOSTPATH
+  elif [ -f "$LOCAL_RESOURCE" ]; then
+    echo "== Copying file $LOCAL_RESOURCE to $SSH_FULL_HOSTPATH=="
+    COMMAND=`{ echo "tee $LOCAL_RESOURCE <<EOF"; cat $LOCAL_RESOURCE; echo "EOF"; }`
+  
+    printf %s "$COMMAND" | ssh $SSH_FULL_HOSTPATH
+  else
+    echo "Please specify required LOCAL_RESOURCE parameters > 'trsshpush SSH_FULL_HOSTPATH LOCAL_RESOURCE'."
+    echo "LOCAL_RESOURCE needs to be a valid file"
+    return -1
   fi
 }
 
-trwgetsafe() {
-  echo "Downloading : wget -O $1 $2"
-  wget -O $1.bak $2
-  mv $1.bak $1  
-  echo "Done : $1"
+trscppush() {
+  if [ -z "$1" ]; then
+    echo "Please specify required SSH_FULL_HOSTPATH parameters > 'trscppush SSH_FULL_HOSTPATH REMOTE_RESOURCE [LOCAL_RESOURCE]'."
+    echo "SSH_FULL_HOSTPATH can be IP, SSH_HOSTNAME or USER@SSH_HOSTNAME"
+    return -1
+  fi
+  if [ -z "$2" ]; then
+    echo "Please specify required SSH_FULL_HOSTPATH parameters > 'trscppush SSH_FULL_HOSTPATH REMOTE_RESOURCE [LOCAL_RESOURCE]'."
+    return -1
+  fi
+  local SSH_FULL_HOSTPATH=$1
+  local LOCAL_RESOURCE=$2
+  local REMOTE_RESOURCE=${3:-$2}
+
+  if [ -d "$LOCAL_RESOURCE" ]; then
+    echo scp -r "$LOCAL_RESOURCE" $SSH_FULL_HOSTPATH:"$REMOTE_RESOURCE"
+    sudo scp -r "$LOCAL_RESOURCE" $SSH_FULL_HOSTPATH:"$REMOTE_RESOURCE"
+  elif [ -f "$LOCAL_RESOURCE" ]; then
+    echo scp "$LOCAL_RESOURCE" $SSH_FULL_HOSTPATH:"$REMOTE_RESOURCE"
+    sudo scp "$LOCAL_RESOURCE" $SSH_FULL_HOSTPATH:"$REMOTE_RESOURCE"
+  else
+    echo "$LOCAL_RESOURCE is not valid"
+    return -1
+  fi
+}
+trscpget() {
+  if [ -z "$1" ]; then
+    echo "Please specify required SSH_FULL_HOSTPATH parameters > 'trscpget SSH_FULL_HOSTPATH REMOTE_RESOURCE [LOCAL_RESOURCE]'."
+    echo "SSH_FULL_HOSTPATH can be IP, SSH_HOSTNAME or USER@SSH_HOSTNAME"
+    return -1
+  fi
+  if [ -z "$2" ]; then
+    echo "Please specify required SSH_FULL_HOSTPATH parameters > 'trscpget SSH_FULL_HOSTPATH REMOTE_RESOURCE [LOCAL_RESOURCE]'."
+    return -1
+  fi
+  local SSH_FULL_HOSTPATH=$1
+  local REMOTE_RESOURCE=$2
+  local LOCAL_RESOURCE=${3:-$2}
+
+  if [ -d "$LOCAL_RESOURCE" ]; then
+    echo scp -r $SSH_FULL_HOSTPATH:$REMOTE_RESOURCE "$LOCAL_RESOURCE"
+    sudo scp -r $SSH_FULL_HOSTPATH:$REMOTE_RESOURCE "$LOCAL_RESOURCE"
+  elif [ -f "$LOCAL_RESOURCE" ]; then
+    echo scp $SSH_FULL_HOSTPATH:$REMOTE_RESOURCE "$LOCAL_RESOURCE"
+    sudo scp $SSH_FULL_HOSTPATH:$REMOTE_RESOURCE "$LOCAL_RESOURCE"
+  else
+    echo "$LOCAL_RESOURCE is not valid"
+    return -1
+  fi
 }
