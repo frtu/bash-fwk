618242a40ae44afae71f3d9dc70b9110e91a2d54
Frederic,Tu <frdtu@paypal.com>
2017-11-25 13:31:15 +0800

    Add some Git functions

--- libs/lib-git.bash
+++ libs/lib-git.bash
@@ -8,24 +8,6 @@ parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
-gdff() {
-  if [ $# -eq 0 ]; then
-    echo "Please specify parameters > 'dff FOLDER_SOURCE FOLDER_TARGET'."
-    return -1
-  fi
-  echo "patch -p$LEVEL -t -i $FILENAME"
-  diff -rupN $1 $2
-}
-gpch() {
-  if [ $# -eq 0 ]; then
-    echo "Please specify parameters > 'pch PATCH_FILE_NAME'."
-    return -1
-  fi
-  local FILENAME=$1
-  local LEVEL=${2:-1}
-  echo "patch -p$LEVEL -t -i $FILENAME"
-  patch -p$LEVEL -t -i $FILENAME
-}
 
 gtag() {
   git tag
@@ -34,34 +16,104 @@ gtagdate() {
   gtag | xargs -L1 git log --pretty=format:"%D %cI" -1 
 }
 
-glog() {
-	local NUMBER=${1:-10}
-	echo "git log --oneline -n $NUMBER"
-	git log --oneline -n $NUMBER
+gbrls() {
+  echo "git branch -a"
+  git branch -a
 }
-greset() {
-	local HASH=${1:-HEAD}
-	echo "git reset --hard $HASH"
-	git reset --hard $HASH
+gbradd() {
+  if [ $# -le 1 ]; then
+      echo "Please supply the BRANCH_NAME to delete as first argument"
+      return -1
+  fi
+  local BRANCH_NAME=$1
+  local REPO_NAME=$2
+
+  if [ -n "$REPO_NAME" ]
+    then
+      if [ "$REPO_NAME" = "origin" ]
+        then
+          local NEW_REPO_NAME=$REPO_NAME
+          local NEW_BRANCH_NAME=$BRANCH_NAME
+        else
+          local NEW_REPO_NAME=repo-$REPO_NAME
+          local NEW_BRANCH_NAME=$NEW_REPO_NAME-$BRANCH_NAME
+      fi
+      echo "git branch $NEW_BRANCH_NAME repo-$REPO_NAME/$BRANCH_NAME"
+      git branch $NEW_BRANCH_NAME $NEW_REPO_NAME/$BRANCH_NAME
+      echo "git checkout $NEW_BRANCH_NAME"
+      git checkout $NEW_BRANCH_NAME
+
+    else
+      echo "git branch $BRANCH_NAME"
+      git branch $BRANCH_NAME
+      echo "git checkout $BRANCH_NAME"
+      git checkout $BRANCH_NAME
+  fi
+  git pull
+}
+gbrrm() {
+  if [ $# -le 0 ]; then
+      echo "Please supply the BRANCH_NAME to delete as first argument"
+      return -1
+  fi
+  local BRANCH_NAME=$1
+
+  echo "git checkout master"
+  git checkout master
+
+  echo "git branch -D $BRANCH_NAME"
+  git branch -D $BRANCH_NAME
+}
+
+gbrremotels() {
+  echo "List all remote branches"
+  git remote -v
+}
+gbrremoteadd() {
+  if [ $# -eq 0 ]; then
+      echo "Please supply argument(s) \"\[REPO_NAME\] PROJECT_NAME\""
+      return -1
+  fi
+  local REPO_NAME=$1
+  local GITHUB_PROJECT=$2
+  local GITHUB_ROOT_URL=${3:-github.com}
+
+  local REMOTE_NAME=repo-$REPO_NAME
+
+  echo "git remote add $REMOTE_NAME git@$GITHUB_ROOT_URL:$REPO_NAME/$GITHUB_PROJECT.git"
+  git remote add $REMOTE_NAME git@$GITHUB_ROOT_URL:$REPO_NAME/$GITHUB_PROJECT.git
+  echo "git fetch $REMOTE_NAME"
+  git fetch $REMOTE_NAME
+
+  gbrremotels
+}
+gbrremoterm() {
+  if [ $# -eq 0 ]; then
+      echo "Please supply argument(s) \"\[REPO_NAME\] PROJECT_NAME\""
+      return -1
+  fi
+  local REMOTE_NAME=remote-$1
+  echo "git remote remove $REMOTE_NAME"
+  git remote remove $REMOTE_NAME
 }
 
 gpatch() {
 	if [ -z "$1" ]
-    	then
-        	local DIFF_NAME=diff
-	    else
-    	    local DIFF_NAME=$1
-  	fi
+  	then
+      	local DIFF_NAME=diff
+    else
+  	    local DIFF_NAME=$1
+	fi
 	echo "git diff > $DIFF_NAME.patch"
 	git diff > $DIFF_NAME.patch
 }
 gpatchapply() {
 	if [ -z "$1" ]
-    	then
-        	local DIFF_NAME=diff
-	    else
-    	    local DIFF_NAME=$1
-  	fi
+  	then
+      	local DIFF_NAME=diff
+    else
+  	    local DIFF_NAME=$1
+	fi
 	
 	if [ -f "$DIFF_NAME.patch" ]; then
 		echo "Applying patch with 'git apply $DIFF_NAME.patch'"
