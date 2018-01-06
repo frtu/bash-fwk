5856f287aff9434b4309a62c5f723a3b5e84350d
Frederic,Tu <frdtu@paypal.com>
2018-01-06 12:41:53 +0800

    Aggregate all services capabilities into a core/services file

---
+++ core/services.bash
@@ -0,0 +1,69 @@
+srv_list() {
+  # http://www.cyberciti.biz/faq/unix-linux-extract-filename-and-extension-in-bash/
+  echo "== List ALL services =="
+  for i in $SERVICE_TEMPLATE_BASH_PREFIX*.bas; do 
+    [ ! -f "$i" ] && break # SKIP the file pattern when no file matches
+    FILENAME=${i##*/}
+    echo ${FILENAME%.*} | cut -d'-' -f2 # Echo BASENAME and keep only the second part after -
+  done
+
+  echo "== Activated services =="
+  for i in $SERVICE_LOCAL_BASH_PREFIX*.bash; do 
+  	[ ! -f "$i" ] && break # SKIP the file pattern when no file matches
+  	FILENAME=${i##*/}
+  	echo ${FILENAME%.*} | cut -d'-' -f2 # Echo BASENAME and keep only the second part after -
+  done
+}
+
+srv_activate() {
+  local SERVICE_BASENAME=$SERVICE_TEMPLATE_BASH_PREFIX$1
+  local SERVICE_TARGET_BASENAME=$SERVICE_LOCAL_BASH_PREFIX$1
+  if [ -f "$SERVICE_BASENAME.bas" ]
+  then
+  	echo "Activate service $1"
+	  cp $SERVICE_BASENAME.bas $SERVICE_TARGET_BASENAME.bash
+  else
+  	echo "'$1' does not exists! Please profile a valide service name using 'srv_list'"
+  fi
+}
+srv_deactivate() {
+  local SERVICE_TARGET_BASENAME=$SERVICE_LOCAL_BASH_PREFIX$1
+  if [ -f "$SERVICE_TARGET_BASENAME.bash" ]
+  then
+  	echo "Deactivate service $1"
+	  rm $SERVICE_TARGET_BASENAME.bash
+  else
+  	echo "'$1' does not exists! Please profile a valide service name using 'srv_list'"
+  fi
+}
+
+lslibs() {
+  ll $LIBS_FOLDER/
+}
+
+enablelib() {
+  if [ $# -eq 0 ]; then
+    echo "Please supply the argument WITHOUT the prefix 'lib-' : LIB_NAME_WITHOUT_PREFIX to enable "
+    lslibs
+    return -1
+  fi
+  if [ ! -f "$LIBS_FOLDER/lib-$1.bash" ]; then
+    echo "Service doesn't exist : $1. Please check lib folder!"
+    lslibs
+    return -1
+  fi
+
+  local LIB_NAME_WITHOUT_PREFIX=$1
+  local TARGET_SERVICE_FILENAME=$LOCAL_SCRIPTS_FOLDER/service-$LIB_NAME_WITHOUT_PREFIX.bash
+
+  echo "Enabling service at $TARGET_SERVICE_FILENAME"
+  echo "import lib-$LIB_NAME_WITHOUT_PREFIX" > $TARGET_SERVICE_FILENAME
+}
+
+enablehadoop() {
+  enablelib hadoop
+}
+enabledockertoolbox() {
+  enablelib dockertoolbox
+}
+
