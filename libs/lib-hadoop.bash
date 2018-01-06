2a7577763718d9dfabb22166df02337e8e2cf670
Frederic,Tu <frdtu@paypal.com>
2018-01-06 13:16:06 +0800

    Add the hadoop script

---
+++ libs/lib-hadoop.bash
@@ -0,0 +1,5 @@
+alias hls='hadoop fs -ls '
+alias hdu='hadoop fs -du -h '
+
+# https://hadoop.apache.org/docs/r2.7.2/hadoop-project-dist/hadoop-hdfs/HdfsQuotaAdminGuide.html
+alias hquota='hadoop fs -count -q -h -v '
