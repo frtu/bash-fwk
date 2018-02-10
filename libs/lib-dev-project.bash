199ef8b98ca8157e8b56f4d553ad18f151b0b516
Frederic,Tu <frdtu@paypal.com>
2018-02-10 19:54:27 +0800

    Add git & mvn command on subfolder using prj cmd prefix

---
+++ libs/lib-dev-project.bash
@@ -0,0 +1,41 @@
+prjmvn() {
+  # MIN NUM OF ARG
+  if [[ "$#" < "1" ]]; then
+      echo "Usage : prjmvn COMMAND . Run 'mvn COMMAND' on each subfolders" >&2
+      return -1
+  fi
+
+  local COMMAND="mvn $@"
+  for directory in */ ; do 
+    cd $directory
+    if [ -f "pom.xml" ]
+      then
+        echo "== $PWD/$directory> $COMMAND =="
+        eval "$COMMAND"
+      else
+        echo "== Skip folder $PWD/$directory because there is no 'pom.xml' file"
+    fi
+    cd ..
+  done
+}
+
+prjgit() {
+  # MIN NUM OF ARG
+  if [[ "$#" < "1" ]]; then
+      echo "Usage : prjgit COMMAND . Run 'git COMMAND' on each subfolders" >&2
+      return -1
+  fi
+
+  local COMMAND="git $@"
+  for directory in */ ; do 
+    cd $directory
+    if [ -d ".git" ]
+      then
+        echo "== $PWD/$directory> $COMMAND =="
+        eval "$COMMAND"
+      else
+        echo "== Skip folder $PWD/$directory because there is no '.git' folder"
+    fi
+    cd ..
+  done
+}
