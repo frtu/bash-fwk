a459ac7af95fe5acd7ea6fd050cbef089b5178ad
Frederic,Tu <frdtu@paypal.com>
2018-03-25 23:40:23 +0800

    Allow to publish message

--- libs/lib-dck-kafka.bash
+++ libs/lib-dck-kafka.bash
@@ -2,6 +2,9 @@ import lib-zk
 
 export ZOOKEEPER_HOSTNAME=zookeeper
 
+export DEFAULT_KAFKA_HOSTNAME=localhost
+export DEFAULT_KAFKA_PORT=9092
+
 kaftopiccreate() {
   usage $# "DCK_IMAGE_NAME" "KAFKA_TOPIC_NAME" "[PARTITION_NUMBER]" "[REPLICATION_FACTOR]"
   ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
@@ -24,6 +27,19 @@ kaftopicdescribe() {
 
   kaftemplate "${DCK_IMAGE_NAME}" "--describe --topic ${KAFKA_TOPIC_NAME}"
 }
+kafproduce() {
+  usage $# "DCK_IMAGE_NAME" "KAFKA_TOPIC_NAME" "MAX_MSG"
+  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
+  if [[ "$?" -ne 0 ]]; then return -1; fi
+
+  local DCK_IMAGE_NAME=$1
+  local KAFKA_TOPIC_NAME=$2
+  local MAX_MSG=$3
+
+  echo "dckbash $DCK_IMAGE_NAME \"kafka-verifiable-producer --topic ${KAFKA_TOPIC_NAME} --max-messages ${MAX_MSG} --broker-list ${DEFAULT_KAFKA_HOSTNAME}:${DEFAULT_KAFKA_PORT}\""
+  dckbash $DCK_IMAGE_NAME "kafka-verifiable-producer --topic ${KAFKA_TOPIC_NAME} --max-messages ${MAX_MSG} --broker-list ${DEFAULT_KAFKA_HOSTNAME}:${DEFAULT_KAFKA_PORT}"
+}
+
 
 kaftemplate() {
   usage $# "DCK_IMAGE_NAME" "MORE_ARG"
