eb9ac998025ef85b170ce6407ebd030eed2233c7
TU Frederic <frederic.tu@gmail.com>
2018-04-01 12:02:27 +0800

    Refactor script using usage & ${} syntax

--- libs/lib-hadoop.bash
+++ libs/lib-hadoop.bash
@@ -4,42 +4,47 @@ alias hdu='hadoop fs -du -h '
 # https://hadoop.apache.org/docs/r2.7.2/hadoop-project-dist/hadoop-hdfs/HdfsQuotaAdminGuide.html
 alias hquota='hadoop fs -count -q -h -v '
 
+alias hput='hdfs dfs -put '
+
 hdirsize() {
-  # MIN NUM OF ARG
-  if [[ "$#" < "1" ]]; then
-      echo "Usage : hdirsize HDFS_FOLDER [HDFS_MORE_ARG]. Run 'hdfs dfs -count -q' on each subfolders of HDFS_FOLDER" >&2
-      echo " HDFS_MORE_ARG : -h = human readable, -v = verbose" >&2
-      return -1
+  usage $# "HDFS_FOLDER" "[OPTIONAL_ARG]"
+  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
+  if [[ "$?" -ne 0 ]]; then 
+    echo " Run 'hdfs dfs -count -q' on each subfolders of HDFS_FOLDER" >&2
+    echo " HDFS_MORE_ARG : -h = human readable, -v = verbose" >&2
+    return -1 
   fi
 
   local HDFS_FOLDER=$1
-  local HDFS_MORE_ARG=${@:2}
+  local OPTIONAL_ARG=${@:2}
 
-  hdircmd $HDFS_FOLDER "hdfs dfs -count -q $HDFS_MORE_ARG"
+  hdirtpl ${HDFS_FOLDER} "hdfs dfs -count -q ${OPTIONAL_ARG}"
 }
 hdirls() {
-  # MIN NUM OF ARG
-  if [[ "$#" < "1" ]]; then
-      echo "Usage : hdirls HDFS_FOLDER [HDFS_MORE_ARG]. Run 'hdfs dfs -ls' on each subfolders of HDFS_FOLDER" >&2
-      return -1
+  usage $# "HDFS_FOLDER" "[OPTIONAL_ARG]"
+  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
+  if [[ "$?" -ne 0 ]]; then 
+    echo "Run 'hdfs dfs -ls [OPTIONAL_ARG]' on each subfolders of HDFS_FOLDER" >&2
+    return -1 
   fi
 
   local HDFS_FOLDER=$1
-  local HDFS_MORE_ARG=${@:2}
+  local OPTIONAL_ARG=${@:2}
 
-  hdircmd $HDFS_FOLDER "hdfs dfs -ls $HDFS_MORE_ARG"
+  hdirtpl ${HDFS_FOLDER} "hdfs dfs -ls ${OPTIONAL_ARG}"
 }
 
-hdircmd() {
-  # MIN NUM OF ARG
-  if [[ "$#" < "2" ]]; then
-      echo "Usage : hdircmd HDFS_FOLDER HDFS_CMD.. . Run 'HDFS_CMD..' on each subfolders of HDFS_FOLDER" >&2
-      return -1
+hdirtpl() {
+  usage $# "HDFS_FOLDER" "HDFS_MANY_ARGS_CMD"
+  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
+  if [[ "$?" -ne 0 ]]; then 
+    echo "Allow to run 'HDFS_MANY_ARGS_CMD..' on each subfolders of HDFS_FOLDER" >&2
+    return -1 
   fi
 
   local HDFS_FOLDER=$1
-  local HDFS_CMD=${@:2}
+  local HDFS_MANY_ARGS_CMD=${@:2}
 
-  echo "hdfs dfs -ls $HDFS_FOLDER | awk -v COMMAND=\"$HDFS_CMD \" '{system(COMMAND \$8)}'"
-  hdfs dfs -ls $HDFS_FOLDER | awk -v COMMAND="$HDFS_CMD " '{system(COMMAND $8)}'
+  echo "hdfs dfs -ls ${HDFS_FOLDER} | awk -v COMMAND=\"$HDFS_MANY_ARGS_CMD \" '{system(COMMAND \$8)}'"
+  hdfs dfs -ls ${HDFS_FOLDER} | awk -v COMMAND="$HDFS_MANY_ARGS_CMD " '{system(COMMAND $8)}'
 }
