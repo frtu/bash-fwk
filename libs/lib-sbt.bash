f9a68e5c74250434136f1c8266f191c529b9068f
Fred <frederic.tu@gmail.com>
2018-08-26 11:09:11 +0800

    [SCALA] create sbt template cmd and improve logs

--- libs/lib-sbt.bash
+++ libs/lib-sbt.bash
@@ -1,3 +1,13 @@
+DEFAULT_BUILD_FILE=build.sbt
+
 sbtscala() {
-  sbt "console"
+  sbttpl "console" $@
+}
+
+sbttpl() {
+  if [ ! -f "${DEFAULT_BUILD_FILE}" ]; then
+  	echo "ATTENTION : Are you sure to run in folder '${PWD}' that doesn't have the file '${DEFAULT_BUILD_FILE}'?"
+  fi
+  echo "${PWD}> sbt $@"
+  sbt $@
 }
