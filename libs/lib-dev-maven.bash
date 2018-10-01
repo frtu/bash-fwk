36758e53be6a133467647b59ec83d6a837c45975
Fred <frederic.tu@gmail.com>
2018-10-01 22:09:25 +0800

    Revert "Allow to use the Maven Archetype project generation"
    This reverts commit 655c699c4a3d119743c7ae058ab7aa84d9911a6a.

--- libs/lib-dev-maven.bash
+++ libs/lib-dev-maven.bash
@@ -6,7 +6,6 @@ MVN_SETTINGS_STANDALONE=STANDALONE
 MVN_SETTINGS_NEXUS_DEFAULT=NEXUS
 
 DEPENDENCY_GAV=com.github.ferstl:depgraph-maven-plugin:3.2.0
-ARCHETYPE_VERSION=0.3.0
 
 mvnsk() { # Skip all tests and enforcer
   mvn -DskipTests -Denforcer.skip $@
@@ -89,40 +88,6 @@ mvnimportjar() {
   fi  
   echo "mvn install:install-file -Dfile=$FILE_PATH -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$ARTIFACT_VERSION -Dpackaging=jar -DgeneratePom=true"
   mvn install:install-file -Dfile=$FILE_PATH -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$ARTIFACT_VERSION -Dpackaging=jar -DgeneratePom=true
-}
-
-mvngenlocal() {
-  usage $# "ARCHETYPE:kafka" "GID" "AID" "[VERSION:0.0.1-SNAPSHOT]" "[EXTRA_PARAM]"
-  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
-  if [[ "$?" -ne 0 ]]; then return -1; fi
-
-  local VERSION=${4:-0.0.1-SNAPSHOT}
-
-  mvngen $1 $2 $3 $VERSION -DarchetypeCatalog=local ${@:5}
-}
-mvngen() {
-  usage $# "ARCHETYPE:kafka" "[GID]" "[AID]" "[VERSION:0.0.1-SNAPSHOT]" "[EXTRA_PARAM]"
-  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
-  if [[ "$?" -ne 0 ]]; then return -1; fi
-
-  local ARCHETYPE=$1
-  local GID=$2
-  local AID=$3
-  local VERSION=${4:-0.0.1-SNAPSHOT}
-
-  local OPTIONAL_ARGS=${@:5}
-  if [ -n "${GID}" ]; then
-    OPTIONAL_ARGS="${OPTIONAL_ARGS} -DgroupId=${GID}"
-  fi
-  if [ -n "${AID}" ]; then
-    OPTIONAL_ARGS="${OPTIONAL_ARGS} -DartifactId=${AID}"
-  fi
-  if [ -n "${VERSION}" ]; then
-    OPTIONAL_ARGS="${OPTIONAL_ARGS} -Dversion=${VERSION}"
-  fi
-
-  echo "mvn archetype:generate -DarchetypeGroupId=com.github.frtu.archetype -DarchetypeArtifactId=${ARCHETYPE}-project-archetype -DarchetypeVersion=${ARCHETYPE_VERSION} ${OPTIONAL_ARGS}"
-  mvn archetype:generate -DarchetypeGroupId=com.github.frtu.archetype -DarchetypeArtifactId=${ARCHETYPE}-project-archetype -DarchetypeVersion=${ARCHETYPE_VERSION} ${OPTIONAL_ARGS}
 }
 
 mvnrepoinit() {
