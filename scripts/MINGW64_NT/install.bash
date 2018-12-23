a3bc162b0a909fe786ef75da53bc31054667d60c
Frederic,Tu <frederic.tu@gmail.com>
2018-12-23 15:57:44 +0100

    FIX enablevboxwin issue

--- scripts/MINGW64_NT/install.bash
+++ scripts/MINGW64_NT/install.bash
@@ -1,14 +1,14 @@
 export PROGRAM_FILES="/c/Program Files"
 
-enablevmwin() {
+enablevboxwin() {
   local VIRTUALBOX_HOME=${1:-$PROGRAM_FILES/Oracle/VirtualBox/}
   echo "VIRTUALBOX_HOME=$VIRTUALBOX_HOME"
 
   if [ -d "$VIRTUALBOX_HOME" ]
     then
-      enablelib vm
-      echo "export VIRTUALBOX_HOME=\"$VIRTUALBOX_HOME\"" >> $SERVICE_SCR_vm
-      echo "export PATH=\$PATH:\"\$VIRTUALBOX_HOME\"" >> $SERVICE_SCR_vm
+      enablelib virtualbox
+      echo "export VIRTUALBOX_HOME=\"$VIRTUALBOX_HOME\"" >> $SERVICE_SCR_virtualbox
+      echo "export PATH=\$PATH:\"\$VIRTUALBOX_HOME\"" >> $SERVICE_SCR_virtualbox
     else
 	  usage $# "VIRTUALBOX_HOME"
 	  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
