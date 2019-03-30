27470308971136e23d3d0b1fd01a1565ac3523db
Tu, Fred <frederic.tu@gmail.com>
2019-03-30 22:39:59 +0800

    Change default port to 80 Add tip to fix py error & usage cmds

--- libs/lib-ssocks.bash
+++ libs/lib-ssocks.bash
@@ -3,14 +3,15 @@ export SS_CONFIG=/etc/shadowsocks.json
 alias ssconfcat='cat $SS_CONFIG'
 
 ssconf() {
-  usage $# "PASSWORD" "[HOSTNAME]"
+  usage $# "PASSWORD" "[HOSTNAME]" "[PORT]"
   ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
   if [[ "$?" -ne 0 ]]; then return -1; fi
 
   local PASSWORD=$1
   local HOSTNAME=${2:-10.140.0.2}
-  
-  echo "{\"server\":\"${HOSTNAME}\",\"server_port\":8080,\"local_port\":0,\"password\":\"${PASSWORD}\",\"timeout\":600,\"method\":\"aes-256-cfb\"}" | sudo tee $SS_CONFIG > /dev/null
+  local PORT=${3:-80}
+
+  echo "{\"server\":\"${HOSTNAME}\",\"server_port\":${PORT},\"local_port\":0,\"password\":\"${PASSWORD}\",\"timeout\":600,\"method\":\"aes-256-cfb\"}" | sudo tee $SS_CONFIG > /dev/null
   ssconfcat
 }
 ssstart() {
--- scripts/Linux/install.bash
+++ scripts/Linux/install.bash
@@ -15,4 +15,17 @@ inst_ss() {
   sudo pip3 install shadowsocks
   
   ssconf ${PASSWORD} ${HOSTNAME}
+
+  STATUS=$?
+  if [ "$STATUS" -eq 0 ]; then
+      echo "Installation Success!"
+      echo "- sudo vi /usr/local/lib/python3.6/dist-packages/shadowsocks/crypto/openssl.py"
+      echo "- Change all : EVP_CIPHER_CTX_cleanup => EVP_CIPHER_CTX_reset"
+      echo ""
+      echo "Launch and Stop application with :"
+      echo "> ssstart"
+      echo "> ssstop"
+    else
+      echo "== Install error, please read logs. ==" >&2
+  fi
 }
