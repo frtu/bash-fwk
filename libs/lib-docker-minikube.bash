c99b0ff6d06fced9b0381e8493f59144ab383ad0
Tu, Fred <frederic.tu@gmail.com>
2019-09-28 21:54:02 +0800

    Allow to define different kubernetes VM in VirtualBox

--- libs/lib-docker-minikube.bash
+++ libs/lib-docker-minikube.bash
@@ -1,6 +1,16 @@
+MINIKUBE_ROOT=~/.minikube
 MINIKUBE_PERSIST_FILE=$LOCAL_SCRIPTS_FOLDER/env-minikube-instance.bash
 
+cdk8s() {
+  cd $MINIKUBE_ROOT
+}
+
 k8mstart() {
+  local IMAGE_NAME=$1
+  if [ -n "$IMAGE_NAME" ]; then
+  	k8mloadpersist $IMAGE_NAME
+  fi
+
   k8mtemplate "start" $@
 }
 k8mssh() {
@@ -24,21 +34,33 @@ k8mrm() {
 k8mtemplate() {
   local EXTRA_PARAMS=${@:2}
 
+  if [ -n "$MINIKUBE_DEFAULT_INSTANCE" ]; then
+    local EXTRA_PARAMS="$EXTRA_PARAMS -p $MINIKUBE_DEFAULT_INSTANCE"
+  fi
+
   echo "minikube $1 ${EXTRA_PARAMS}"
   minikube $1 ${EXTRA_PARAMS}
 }
 
 k8mload() {
-	local IMAGE_NAME=${1:-$MINIKUBE_NAME}
-	echo "${FUNCNAME} ${IMAGE_NAME}"
-	eval $(minikube docker-env)
+  local IMAGE_NAME=$1
+  echo "${FUNCNAME} ${IMAGE_NAME}"
+
+  if [ -n "$1" ]; then
+    local OPTIONAL_ARGS="$OPTIONAL_ARGS -p $1"
+  fi
+  minikube docker-env ${OPTIONAL_ARGS} | grep "DOCKER_HOST"
+  eval $(minikube docker-env ${OPTIONAL_ARGS})
 }
 k8mloadpersist() {
-  local IMAGE_NAME=${1:-$MINIKUBE_NAME}
-  k8mload $IMAGE_NAME
+  local IMAGE_NAME=$1
+  if [ -n "$1" ]; then
+    echo "Persiting MINIKUBE_DEFAULT_INSTANCE=$IMAGE_NAME!"
+    echo "export MINIKUBE_DEFAULT_INSTANCE=$IMAGE_NAME" > $MINIKUBE_PERSIST_FILE
+    source $MINIKUBE_PERSIST_FILE
+  fi
 
-  echo "Persiting MINIKUBE_DEFAULT_INSTANCE=$IMAGE_NAME!"
-  echo "export MINIKUBE_DEFAULT_INSTANCE=$IMAGE_NAME" > $MINIKUBE_PERSIST_FILE
+  k8mload $IMAGE_NAME
 }
 k8mloadunset() {
   echo "unset MINIKUBE_DEFAULT_INSTANCE"
