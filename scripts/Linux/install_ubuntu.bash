568ab96b2399ef47bb16ddfc827e5fd5e7dd30a6
Tu, Fred <frederic.tu@gmail.com>
2019-10-06 23:23:29 +0800

    [REFACTOR] Move install docker into the according bash

--- scripts/Linux/install_ubuntu.bash
+++ scripts/Linux/install_ubuntu.bash
@@ -33,6 +33,18 @@ inst_youtube() {
   sudo apt-get -y install youtube-dl
 }
 
+inst_docker_ubuntu() {
+  # FOLOW : https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-docker-ce
+  sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo apt-key fingerprint 0EBFCD88
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce
+  sudo docker run hello-world
+  sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+}
 inst_minikube_standalone_ubuntu() {
   echo "Install Docker to allow K8S to work on standalone"
   echo "-------------------------------------------------"
