f7884ad9c8c7f2a473a10f95fdce7659d383f089
Tu, Fred <frederic.tu@gmail.com>
2019-10-13 00:08:34 +0800

    [VM] allow to create vm folder & sudo docker-compose

--- libs/lib-vm.bash
+++ libs/lib-vm.bash
@@ -2,6 +2,9 @@ VM_ROOT_FOLDER=~/VMs
 VM_ARCHIVE_FOLDER=$VM_ROOT_FOLDER/_archives_
 
 cdvm() {
-	mkdir -p $VM_ROOT_FOLDER
-	cd $VM_ROOT_FOLDER
+  mkdir -p $VM_ROOT_FOLDER
+  cd $VM_ROOT_FOLDER
+}
+mkvm() {
+  mkdir -p $VM_ARCHIVE_FOLDER	
 }
