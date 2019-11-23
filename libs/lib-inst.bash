7d02b4ddcdca075de980a8b1ab4ef914b70d27a3
Tu, Fred <frederic.tu@gmail.com>
2019-11-23 16:20:36 +0800

    [lib-inst] fix

--- libs/lib-inst.bash
+++ libs/lib-inst.bash
@@ -1,7 +1,7 @@
 if [[ -z "$INSTALL_TOOL" ]] ; then INSTALL_TOOL="apt -y" ; fi
 if [[ -z "$UNINSTALL_TOOL" ]] ; then UNINSTALL_TOOL="apt purge" ; fi
 
-if [[ -z "$CHECK_SUDO" && $(id -u) -ne 0 ]] ; then INSTALL_TOOL="sudo $INSTALL_TOOL" ; then UNINSTALL_TOOL="sudo $UNINSTALL_TOOL" ; fi
+if [[ -z "$CHECK_SUDO" && $(id -u) -ne 0 ]] ; then INSTALL_TOOL="sudo $INSTALL_TOOL" ; UNINSTALL_TOOL="sudo $UNINSTALL_TOOL" ; fi
 
 inst() {
   usage $# "PACKAGE"
