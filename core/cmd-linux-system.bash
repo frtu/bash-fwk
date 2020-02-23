66ae7515395cc8470a2f2811a5da4e6b38bfaef3
Tu, Fred <frederic.tu@gmail.com>
2020-02-23 23:02:56 +0800

    Change return -1 to return 1

--- core/cmd-linux-system.bash
+++ core/cmd-linux-system.bash
@@ -12,14 +12,14 @@ binln() {
   # MIN NUM OF ARG
   if [[ "$#" < "2" ]]; then
     echo "Please provide a folder='$1' and a command filename='$2'" >&2
-    return -1
+    return 1
   fi
   ln -s "$1/$2" "$USR_BIN/$2"	
 }
 binappend() {
   usage $# "NEW_PATH_FOLDER"
   ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
-  if [[ "$?" -ne 0 ]]; then return -1; fi
+  if [[ "$?" -ne 0 ]]; then return 1; fi
 
   local NEW_PATH_FOLDER=$1
   echo "export PATH=\$PATH:\"$NEW_PATH_FOLDER\"" >> ~/.bash_profile
@@ -53,7 +53,7 @@ tcp() {
   ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
   if [[ "$?" -ne 0 ]]; then 
     ifconfig
-    return -1
+    return 1
   fi
 
   if [ "$1" == "any" ]
@@ -90,7 +90,7 @@ envrm() {
     echo "If you don't know any names run 'envls KEYWORD' or 'envls'." >&2
     echo "" >&2
     envls
-    return -1
+    return 1
   fi
 
   local ENV_NAME_TO_REMOVE=$1
