bc78e6ab397236b2a0829143beb32469bb80aacb
Tu, Fred <frederic.tu@gmail.com>
2021-01-23 00:06:57 +0800

    Adding a few HADOOP vars

--- libs/lib-hadoop-admin.bash
+++ libs/lib-hadoop-admin.bash
@@ -4,6 +4,11 @@ export HADOOP_BIN=${HADOOP_HOME}/bin
 export HADOOP_CONF_DIR=${HADOOP_HOME}/etc/hadoop
 export HADOOP_LOG_DIR=${HADOOP_HOME}/logs
 
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export YARN_HOME=$HADOOP_HOME
+
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+
 alias hcd='cd ${HADOOP_HOME}'
 alias hcdconf='cd ${HADOOP_CONF_DIR}'
 alias hcdlog='cd ${HADOOP_LOG_DIR}'
