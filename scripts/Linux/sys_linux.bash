7e3effd9e74fa72505f6da37e6c11cf15502d39f
Tu, Fred <frederic.tu@gmail.com>
2022-02-12 17:00:08 +0800

    alpine : Decouple inst_net into centos & debian & enable from alpine

--- scripts/Linux/sys_linux.bash
+++ scripts/Linux/sys_linux.bash
@@ -1,18 +1,20 @@
 if [ -f /etc/redhat-release ]; then
+  import lib-inst
+  import lib-systemctl
   source $SCRIPTS_FOLDER/ext_centos
 fi
 if [ -f /etc/lsb-release ]; then
+  import lib-inst
+  import lib-systemctl
   source $SCRIPTS_FOLDER/ext_debian
   source $SCRIPTS_FOLDER/ext_ubuntu
 fi
-import lib-inst
-import lib-systemctl
+if [ -f /etc/alpine-release ]; then
+  source $SCRIPTS_FOLDER/ext_alpine
+fi
 
 inst_proc() {
   inst procps
-}
-inst_net() {
-  inst net-tools nmap telnet curl ${NET_PKG_EXTRA}
 }
 inst_pip() {
   inst python3-pip
