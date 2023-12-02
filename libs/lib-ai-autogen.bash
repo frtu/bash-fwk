6cad65e8101d0f2aa78d5c4d7027b422c9952280
Tu, Fred <frederic.tu@gmail.com>
2023-12-02 12:32:54 +0800

    feat: adding autogen lib

---
+++ libs/lib-ai-autogen.bash
@@ -0,0 +1,31 @@
+import lib-ai-ollama
+
+AUTOGEN_LIB="autogen"
+
+agcreate() {
+ pcenvcreate ${AUTOGEN_LIB} python=3.11
+ aginst
+}
+agactivate() {
+  pcenv ${AUTOGEN_LIB}
+}
+
+aginst() {
+  echo "pip install pyautogen"
+  pip install pyautogen
+
+  echo "pip install litellm"
+  pip install litellm
+}
+agmodel() {
+  usage $# "MODEL"
+  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
+  if [[ "$?" -ne 0 ]]; then 
+    olls
+    return 1;
+  fi
+
+  local MODEL=$1
+  olpull ${MODEL}
+  litellm --model ollama/${MODEL}
+}
