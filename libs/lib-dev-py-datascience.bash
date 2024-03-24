fbe628300018a7f902f7e6b8c6469173f3bf1efd
Tu, Fred <frederic.tu@gmail.com>
2024-03-24 18:27:41 +0800

    feat: adding dsinstautogpt

--- libs/lib-dev-py-datascience.bash
+++ libs/lib-dev-py-datascience.bash
@@ -26,6 +26,14 @@ dsinsttransformers() {
   pcinst transformers
   pcinst huggingface_hub
 }
+dsinstautogpt() {
+  usage $# "[VERSION:0.4.2]"
+  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
+  if [[ "$?" -ne 0 ]]; then return 1; fi
+
+  local VERSION=${1:-0.4.2}
+  BUILD_CUDA_EXT=0 pip install "git+https://github.com/PanQiWei/AutoGPTQ.git@v${VERSION}"
+}
 # https://pytorch.org/get-started/locally/
 dsinstpytorch() {
   dsinstrepopytorch pytorch::pytorch torchvision torchaudio
