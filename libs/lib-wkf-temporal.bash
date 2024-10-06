2a81217f402cda64a45e9fbb845517dd9ae347fc
Tu, Fred <frederic.tu@gmail.com>
2024-10-06 18:12:25 +0800

    feat: lib-wkf-temporal - ppinst_temporal

--- libs/lib-wkf-temporal.bash
+++ libs/lib-wkf-temporal.bash
@@ -1,12 +1,18 @@
+import lib-dev-python
+
 alias t='temporal'
 alias tw='temporal workflow'
 alias ts='temporal server start-dev'
+alias tspersist='ts --db-filename'
+alias tsport='ts --ui-port'
 alias tsdb='temporal server start-dev --db-filename ~/temporal.db'
 
 # send process to background so you can continue using the terminal
 alias tsbg='temporal server start-dev &> /dev/null & disown'
 
 inst_temporal() {
-    echo "brew install temporal"
-    brew install temporal
+  inst temporal
+}
+ppinst_temporal() {
+  ppinst temporal
 }
