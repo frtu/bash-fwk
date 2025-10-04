93ddbf1a4e80a4d322fef256d9e4c0d087b1c012
Tu, Fred <frederic.tu@gmail.com>
2025-10-04 17:35:22 +0800

    feat: lib-dev-maven-archetype - add mvngenflink Refactor to use mvngentpl

--- libs/lib-dev-maven-archetype.bash
+++ libs/lib-dev-maven-archetype.bash
@@ -19,43 +19,54 @@ mvngen() {
   if [[ "$?" -ne 0 ]]; then return 1; fi
 
   local ARCHETYPE=$1
-  local AID=$2
-  local GID=${3:-$DEFAULT_GID}
-  local VERSION=${4:-0.0.1-SNAPSHOT}
-
-  local OPTIONAL_ARGS=${@:5}
-  if [ -n "${GID}" ]; then
-    OPTIONAL_ARGS="${OPTIONAL_ARGS} -DgroupId=${GID}"
-  fi
-  if [ -n "${AID}" ]; then
-    OPTIONAL_ARGS="${OPTIONAL_ARGS} -DartifactId=${AID}"
-  fi
-  if [ -n "${VERSION}" ]; then
-    OPTIONAL_ARGS="${OPTIONAL_ARGS} -Dversion=${VERSION}"
-  fi
+  local OPTIONAL_ARGS=${@:2}
 
   echo "= Override version using 'enablemvngen [ARCHETYPE_VERSION]' current ARCHETYPE_VERSION=${ARCHETYPE_VERSION} ="
 
-  echo "mvn archetype:generate -DarchetypeGroupId=com.github.frtu.archetype -DarchetypeArtifactId=${ARCHETYPE}-project-archetype -DarchetypeVersion=${ARCHETYPE_VERSION} ${OPTIONAL_ARGS}"
-  mvn archetype:generate -DarchetypeGroupId=com.github.frtu.archetype -DarchetypeArtifactId=${ARCHETYPE}-project-archetype -DarchetypeVersion=${ARCHETYPE_VERSION} ${OPTIONAL_ARGS}
+  mvngentpl "com.github.frtu.archetype" "${ARCHETYPE}-project-archetype" "${ARCHETYPE_VERSION}" ${OPTIONAL_ARGS}
+}
 
-  cd "${AID}"
-  mvn compile
+# https://nightlies.apache.org/flink/flink-docs-release-2.1/docs/dev/configuration/overview/
+mvngenflink() {
+  usage $# "FLINK_ARCHETYPE_VERSION" "AID" "GID" "[VERSION:0.0.1-SNAPSHOT]"
+  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
+  if [[ "$?" -ne 0 ]]; then 
+    echo "= Check version at https://search.maven.org/artifact/org.apache.flink/flink-quickstart-java =" >&2 
+    return 1
+  fi
+
+  local FLINK_ARCHETYPE_VERSION=${1:-2.1.0}
+  mvngentpl "org.apache.flink" "flink-quickstart-java" "${FLINK_ARCHETYPE_VERSION}" ${@:2}
+
+  echo "= See follow up tutorial at https://nightlies.apache.org/flink/flink-docs-release-2.1/docs/dev/configuration/overview/"
 }
 # https://github.com/openjdk/jmh
 mvngenbenchmark() {
   # https://search.maven.org/artifact/org.openjdk.jmh/jmh-kotlin-benchmark-archetype
-  usage $# "ARCHETYPE:java|kotlin|groovy|scala|simple" "[AID]" "[GID]" "[VERSION:0.0.1-SNAPSHOT]" "[JMH_ARCHETYPE_VERSION:1.32]" "[EXTRA_PARAM]"
+  usage $# "ARCHETYPE:java|kotlin|groovy|scala|simple" "AID" "GID" "[VERSION:0.0.1-SNAPSHOT]" "[JMH_ARCHETYPE_VERSION:1.32]"
   ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
   if [[ "$?" -ne 0 ]]; then return 1; fi
 
   local ARCHETYPE=$1
-  local AID=$2
-  local GID=${3:-$DEFAULT_GID}
-  local VERSION=${4:-0.0.1-SNAPSHOT}
+  local OPTIONAL_ARGS=${@:2}
   local JMH_ARCHETYPE_VERSION=${5:-1.32}
 
-  local OPTIONAL_ARGS=${@:5}
+  mvngentpl "org.openjdk.jmh" "jmh-${ARCHETYPE}-benchmark-archetype" "${JMH_ARCHETYPE_VERSION}" ${OPTIONAL_ARGS} "-DarchetypeCatalog=local" "-DinteractiveMode=false"
+}
+
+mvngentpl() {
+  usage $# "FULL_GROUP_ID" "ARCHETYPE_FULL:base-project-archetype" "FULL_ARCHETYPE_VERSION" "AID" "[GID:${DEFAULT_GID}]" "[VERSION:0.0.1-SNAPSHOT]" "[EXTRA_PARAM]"
+  ## Display Usage and exit if insufficient parameters. Parameters prefix with [ are OPTIONAL.
+  if [[ "$?" -ne 0 ]]; then return 1; fi
+
+  local FULL_GROUP_ID=$1
+  local FULL_ARCHETYPE=$2
+  local FULL_ARCHETYPE_VERSION=$3
+  local AID=$4
+  local GID=${5:-$DEFAULT_GID}
+  local VERSION=${6:-0.0.1-SNAPSHOT}
+
+  local OPTIONAL_ARGS=${@:7}
   if [ -n "${GID}" ]; then
     OPTIONAL_ARGS="${OPTIONAL_ARGS} -DgroupId=${GID}"
   fi
@@ -66,8 +77,11 @@ mvngenbenchmark() {
     OPTIONAL_ARGS="${OPTIONAL_ARGS} -Dversion=${VERSION}"
   fi
 
-  echo "mvn archetype:generate -DarchetypeGroupId=org.openjdk.jmh -DarchetypeArtifactId=jmh-${ARCHETYPE}-benchmark-archetype -DarchetypeVersion=${JMH_ARCHETYPE_VERSION} ${OPTIONAL_ARGS}  -DarchetypeCatalog=local -DinteractiveMode=false"
-  mvn archetype:generate -DarchetypeGroupId=org.openjdk.jmh -DarchetypeArtifactId=jmh-${ARCHETYPE}-benchmark-archetype -DarchetypeVersion=${JMH_ARCHETYPE_VERSION} ${OPTIONAL_ARGS}  -DarchetypeCatalog=local -DinteractiveMode=false
+  echo "mvn archetype:generate -DarchetypeGroupId=${FULL_GROUP_ID} -DarchetypeArtifactId=${FULL_ARCHETYPE} -DarchetypeVersion=${FULL_ARCHETYPE_VERSION} ${OPTIONAL_ARGS}"
+  mvn archetype:generate -DarchetypeGroupId=${FULL_GROUP_ID} -DarchetypeArtifactId=${FULL_ARCHETYPE} -DarchetypeVersion=${FULL_ARCHETYPE_VERSION} ${OPTIONAL_ARGS}
+
+  cd "${AID}"
+  mvn compile
 }
 mvninst() {
   echo "mvn clean install"
